c538189435bca496082916e10e4a1546528a2642
Hanna Co <hannaco@ucla.edu>
2023-11-17 19:03:37 -0800

    fixing bugs and fake bump mapping DONT WORRY ILL FIX IT

--- shapes/asteroid.js
+++ shapes/asteroid.js
@@ -11,6 +11,30 @@ const {Subdivision_Sphere, Cube, Axis_Arrows, Textured_Phong, Windmill, Phong_Sh
 export class Asteroid extends Shape {
     constructor() {
         super("position", "normal", "texture_coord");
-        Subdivision_Sphere.prototype.make_flat_shaded_version().insert_transformed_copy_into(this, [1])
+        Subdivision_Sphere.prototype.make_flat_shaded_version().insert_transformed_copy_into(this, [2])
     }
 }
+
+const Fake_Bump_Map = defs.Fake_Bump_Map =
+    class Fake_Bump_Map extends Textured_Phong {
+        // **Fake_Bump_Map** Same as Phong_Shader, except adds a line of code to
+        // compute a new normal vector, perturbed according to texture color.
+        fragment_glsl_code() {
+            // ********* FRAGMENT SHADER *********
+            return this.shared_glsl_code() + `
+                varying vec2 f_tex_coord;
+                uniform sampler2D texture;
+        
+                void main(){
+                    // Sample the texture image in the correct place:
+                    vec4 tex_color = texture2D( texture, f_tex_coord );
+                    // if( tex_color.w < .01 ) discard;
+                    // Slightly disturb normals based on sampling the same image that was used for texturing:
+                    vec3 bumped_N  = N + tex_color.rgb - 0.5*vec3(1,1,1);
+                    // Compute an initial (ambient) color:
+                    gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                    // Compute the final color with contributions from lights:
+                    gl_FragColor.xyz += phong_model_lights( normalize( bumped_N ), vertex_worldspace );
+                  } `;
+        }
+    }
--- space.js
+++ space.js
@@ -1,13 +1,8 @@
 import {defs, tiny} from './examples/common.js';
-<<<<<<< Updated upstream
-import {Asteroid} from './asteroid.js';
-=======
 import {Asteroid} from './shapes/asteroid.js';
-import {Heart} from './shapes/heart.js'
->>>>>>> Stashed changes
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
 export class Space extends Scene {
@@ -16,9 +11,9 @@ export class Space extends Scene {
         super();
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
-            asteroid: new Asteroid(),
-            // asteroid: new defs.Subdivision_Sphere(4)
-<<<<<<< Updated upstream
+            // asteroid: new Asteroid(),
+            asteroid: new defs.Subdivision_Sphere(5),
+            black_hole: new defs.Capped_Cylinder(20, 20),
             heart_part: new defs.Cube(),
             face: new defs.Capped_Cylinder(20, 20),
             smile: new defs.Closed_Cone(20, 20),
@@ -27,24 +22,24 @@ export class Space extends Scene {
             solar_panel2: new defs.Cube(),
             satellite_head: new defs.Cone_Tip(20, 20),
             satellite_tail: new defs.Subdivision_Sphere(8),
-=======
-            heart: new Heart(),
-            satellite: new defs.Cylindrical_Tube(20, 20),
-            black_hole: new defs.Capped_Cylinder(20, 20)
->>>>>>> Stashed changes
         };
 
         // *** Materials
         this.materials = {
-            asteroid: new Material(new defs.Phong_Shader(),
-                {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#4b4e52")}),
+            asteroid_flat: new Material(new defs.Textured_Phong(), {
+                color: hex_color("#3f4145"),
+                ambient: 0.8, diffusivity: 0, specularity: 0.2, texture: new Texture("assets/asteroid.png")
+            }),
+            asteroid: new Material(new defs.Fake_Bump_Map(), {
+                color: hex_color("#3f4145"),
+                ambient: 0.8, diffusivity: 0, specularity: 0.2, texture: new Texture("assets/asteroid.png")
+            }),
             heart: new Material(new defs.Phong_Shader(),
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#880808")}),
             face: new Material(new defs.Phong_Shader(),
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#000000")}),
             satellite: new Material(new defs.Phong_Shader(),
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#F5F5DC")}),
-<<<<<<< Updated upstream
             solar_panel1: new Material(new defs.Phong_Shader(),
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#0047ab")}),
             solar_panel2: new Material(new defs.Phong_Shader(),
@@ -53,10 +48,8 @@ export class Space extends Scene {
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#4b4e52")}),
             satellite_tail: new Material(new defs.Phong_Shader(),
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#FFFFFF")}),
-=======
             black_hole: new Material(new defs.Phong_Shader(),
                 {ambient: 0.8, diffusivity: 0.5, specularity: 0.2, color: hex_color("#ffffff")}),
->>>>>>> Stashed changes
         }
 
         this.background_colors = [hex_color("#000000"), hex_color("#000435"), hex_color("#36013f")]
@@ -174,11 +167,16 @@ export class Space extends Scene {
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
 
-        const light_position = vec4(0, 0, 0, 1);
-        // program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 10**3)];
+        const light_position = vec4(5, -2, 0, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 10**10)];
 
         let model_transform = Mat4.identity();
         this.spawn_objects(t, context, program_state, model_transform);
+
+        // this.shapes.asteroid.draw(context, program_state, model_transform.times(Mat4.translation(-5, 0, 0)).times(Mat4.scale(3, 4.5, 3)).times(Mat4.rotation(Math.PI * (t*0.1), 0, 1, 0)), this.materials.asteroid)
+        // this.shapes.asteroid.draw(context, program_state, model_transform.times(Mat4.translation(5, 0, 0)).times(Mat4.scale(3, 4.5, 3)).times(Mat4.rotation(Math.PI * (t*0.1), 0, 1, 0)), this.materials.asteroid_flat)
+        // this.shapes.asteroid.draw(context, program_state, model_transform.times(Mat4.translation(-5, 0, 0)).times(Mat4.scale(3, 4.5, 3)).times(Mat4.rotation(Math.PI * (t*0.1), 0, 1, 0)), this.materials.asteroid)
+
 
         let black_hole_transform = model_transform
         black_hole_transform = black_hole_transform.times(Mat4.scale(4, 1, 0.25)).times(Mat4.translation(3, 0, 0))

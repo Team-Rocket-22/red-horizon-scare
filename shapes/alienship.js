b206bab7e8f12cf02be7aeb3e2a6ef1f6efeec0e
gpham2 <giangphamgia03@gmail.com>
2023-11-20 03:31:19 -0800

    large commit

---
+++ shapes/alienship.js
@@ -0,0 +1,19 @@
+import {defs, tiny} from '../examples/common.js';
+
+const {
+    Shape,
+} = tiny;
+const {
+    Capped_Cylinder, Cone_Tip, Subdivision_Sphere
+} = defs;
+
+// export class AlienShip extends Shape {
+   
+//     constructor() {
+//         // Create a disk-shaped alien ship
+//         super("position", "normal", "texture_coord");
+
+//         Cone_Tip.prototype.make_flat_shaded_version().insert_transformed_copy_into(this, [0])
+
+//     }
+// }
